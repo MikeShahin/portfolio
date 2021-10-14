6fb87d9a19b8c2ccf2da936f86820d3e441edea2
MikeShahin <gobias101@gmail.com>
2021-10-14 15:16:02 -0700

    cleaned up setup files/started project object for data

---
+++ src/projects.js
@@ -0,0 +1,12 @@
+const projects = [
+    { name: "Rel-Event", 
+    language: "Ruby", 
+    description: "A command line app that fetches events from an API based on the users preferences", 
+    picture: "url", 
+    video: "url" }, 
+    { name: "", 
+    language: "", 
+    description: "", 
+    picture: "", 
+    video: "" }
+]
