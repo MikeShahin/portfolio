b4a03a229945172d3cedda5ff08a8d82a894333e
MikeShahin <gobias101@gmail.com>
2021-10-21 12:37:17 -0700

    added class names to project divs

--- src/projectsCard.js
+++ src/projectsCard.js
@@ -8,14 +8,14 @@ const ProjectsCard = () => {
         <>  
         <h2>Main Projects:</h2>   
             {mainProjects.map(({ name, language, description, picture, video }) => (
-                <div>
+                <div className="main-projects">
                     <p key={name}>Name: {name},  Language: {language} </p>
                 </div>
             ))}
 
         <h2>Side Projects: </h2>
             {sideProjects.map(({ name, language, description, picture, video }) => (
-                <div>
+                <div className="side-projects">
                     <p key={name}>Name: {name},  Language: {language} </p>
                 </div>
             ))}
