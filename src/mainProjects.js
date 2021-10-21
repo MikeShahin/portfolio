c5f694b23fab0f81a3af04351e8f76ff7ab88033
MikeShahin <gobias101@gmail.com>
2021-10-21 12:35:15 -0700

    added section opn page for main and side projects

--- src/mainProjects.js
+++ src/mainProjects.js
@@ -37,28 +37,4 @@ const mainProjects = [
 
 ]
 
-const sideProjects = [
-    { name: "Rel-Event", 
-    language: "Ruby", 
-    description: "A CLI app allows users to use keywords to search for any type of event they can imagine. The program takes a keyword (like concert, art, food, or even moms)and a location (can be a city, state, zip, country, etc) and presents the user with a list of events showing the title, time/date, and location.",
-    github: "https://github.com/MikeShahin/REL-Event", 
-    picture: "", 
-    video: "" },
-
-    { name: "News Aggregator", 
-    language: "JavaScript", 
-    description: "This is a project I did following a General Assembly class on JavaScript. It pulls news articles from a variety of API sources and shows them to the user.",
-    github: "https://github.com/MikeShahin/News-aggregator-project/blob/master/README.md", 
-    picture: "", 
-    video: "" },
-
-    { name: "Mikebot", 
-    language: "Javascript/CoffeeScript", 
-    description: "This project was to create a bot for slack. I created it so that users can get music suggestions, play games, and ask it simple math problems",
-    github: "https://github.com/MikeShahin/Mikebot", 
-    picture: "", 
-    video: "" }
-
-]
-
 export default mainProjects
--- src/projectsCard.js
+++ src/projectsCard.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import mainProjects from './mainProjects'
+import sideProjects from './sideProjects'
 
 const ProjectsCard = () => {
 
     return(
-        <>     
+        <>  
+        <h2>Main Projects:</h2>   
             {mainProjects.map(({ name, language, description, picture, video }) => (
+                <div>
+                    <p key={name}>Name: {name},  Language: {language} </p>
+                </div>
+            ))}
+
+        <h2>Side Projects: </h2>
+            {sideProjects.map(({ name, language, description, picture, video }) => (
                 <div>
                     <p key={name}>Name: {name},  Language: {language} </p>
                 </div>
