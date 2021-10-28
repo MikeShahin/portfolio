caf18e1a9ba521f7be3605e20b85d966f1ab6190
MikeShahin <gobias101@gmail.com>
2021-10-28 13:40:03 -0700

    Update bio.js

--- src/bio.js
+++ src/bio.js
@@ -4,7 +4,11 @@ const Bio = () => {
 
     return(
         <div>
-
+            <p>
+                Hello, I'm Mike! Welcome to my portfolio. I graduated from San Francisco State University with a B.S. in Mechanical Engineering. I then worked a few years in project design, first designing household goods, then designing high pressure liquid chromatography valves used in medical research.
+                From there I started my own business which was a dream of mine for a long time, the business was doing well until Covid hit and we had to shut down operations. During Covid I followed my lifelong passion of learning how to program and now hope to shift into a coding career.
+                The following are my greatest projects I have created during my learn-to-code journey, I hope you enjoy. You can also check out my Github (linked above) to see all my projects and coding exercises.
+            </p>
         </div>
     )
 }
