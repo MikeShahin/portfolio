1fcb25f716fbc63bace969a04d7688f16bf6c140
MikeShahin <gobias101@gmail.com>
2021-11-04 13:40:43 -0700

    updated format and css

--- src/projectsCard.js
+++ src/projectsCard.js
@@ -6,23 +6,33 @@ const ProjectsCard = () => {
 
     return(
         <>  
-        <h2>Main Projects:</h2>   
+        <h1>Main Projects:</h1>   
             {mainProjects.map(({ name, language, description, picture, video }) => (
-                <div className="main-projects">
-                    <h4>Name: {name}</h4>
-                    <h6>Language: {language}</h6>
-                    <p>Description: {description}</p>
-                    <img src={picture} alt="project pic" height="200"/>
-                </div>
+                <>
+                    <div className="main-projects">
+                        <div>
+                        <h2>{name}</h2>
+                        <img src={picture} alt="project pic" height="200"/>
+                        <h6>Language: {language}</h6>
+                        </div>
+                        <p>Description: {description}</p>
+                    </div>
+                    <br>
+                    </br>
+                </>
             ))}
 
         <h2>Side Projects: </h2>
             {sideProjects.map(({ name, language, description, picture, video }) => (
-                <div className="side-projects">
-                    <h4>Name: {name}</h4>
-                    <h6>Language: {language}</h6>
-                    <p>Description: {description}</p>
-                </div>
+                <>
+                    <div className="side-projects">
+                        <h4>Name: {name}</h4>
+                        <h6>Language: {language}</h6>
+                        <p>Description: {description}</p>
+                    </div>
+                    <br>
+                    </br>
+                </>
             ))}
         </>
     )
