9c3d1abb85ccb34e4eda628d3bc16e6fb603de86
MikeShahin <gobias101@gmail.com>
2021-11-18 11:35:31 -0800

    changed link styling

--- src/header.js
+++ src/header.js
@@ -19,8 +19,8 @@ class Header extends React.Component {
         <a href="#">Mike Shahindoust</a>
         <a href="https://github.com/MikeShahin">Github</a>
         <a href="https://www.linkedin.com/in/mike-shahindoust-499665213/">Linkedin</a>
-        <a href="mailto: MikeShahindoust@gmail.com">Contact</a>
         <a href="https://docs.google.com/document/d/15PWNIgXrqLhvkNCyNYZ5aowhhNhbtVIOuXLWwP0C6Pc/edit?usp=sharing">Resume</a>
+        <a href="mailto: MikeShahindoust@gmail.com">Contact</a>
       </div>
     )
   }
