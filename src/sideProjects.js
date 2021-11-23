18fac68eafb818cf25d8b01b5871e2fcaba5e04a
MikeShahin <gobias101@gmail.com>
2021-11-23 11:36:15 -0800

    cleaned up code

--- src/sideProjects.js
+++ src/sideProjects.js
@@ -4,22 +4,22 @@ const sideProjects = [
     language: "Ruby", 
     description: "A CLI app allows users to use keywords to search for any type of event they can imagine. The program takes a keyword (like concert, art, food, or even moms)and a location (can be a city, state, zip, country, etc) and presents the user with a list of events showing the title, time/date, and location.",
     github: "https://github.com/MikeShahin/REL-Event", 
-    picture: "https://i.imgur.com/ljDkRId.png", 
-    video: "" },
+    picture: "https://i.imgur.com/ljDkRId.png" 
+    },
     
     { name: "Mikebot", 
     language: "Javascript/CoffeeScript", 
     description: "This project was to create a bot for slack. I created it so that users can get music suggestions, play games, and ask it simple math problems",
     github: "https://github.com/MikeShahin/Mikebot", 
-    picture: "https://camo.githubusercontent.com/0886f52710045685ab584e4966072756bf67b7e4184c3f30b436630f9329fd6f/68747470733a2f2f692e696d6775722e636f6d2f5138686a4768382e706e67", 
-    video: "" },
+    picture: "https://camo.githubusercontent.com/0886f52710045685ab584e4966072756bf67b7e4184c3f30b436630f9329fd6f/68747470733a2f2f692e696d6775722e636f6d2f5138686a4768382e706e67" 
+    },
     
     { name: "News Aggregator", 
     language: "JavaScript", 
     description: "This is a project I did following a General Assembly class on JavaScript. It pulls news articles from a variety of API sources and shows them to the user.",
     github: "https://github.com/MikeShahin/News-aggregator-project/blob/master/README.md", 
-    picture: "https://i.imgur.com/d2iZeEt.png", 
-    video: "" }
+    picture: "https://i.imgur.com/d2iZeEt.png"
+    }
 
 ]
 
